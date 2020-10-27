6c1b06af5b21319ae8bb42f4f9d4ffa88efa6c31
Ulric Todman <rick.sinclair.nyc@gmail.com>
2020-10-27 02:16:39 -0400

    fixed project name

--- src/main/java/com/ulrictodman/portfolioapi/PortfolioApiApplication.java
+++ src/main/java/com/ulrictodman/portfolioapi/PortfolioApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactserviceApplication {
+public class PortfolioApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContactserviceApplication.class, args);
+        SpringApplication.run(PortfolioApiApplication.class, args);
     }
 
 }
